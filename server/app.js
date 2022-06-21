62c44512239d8f54a4adab6601b4fe0670519c1f
FrederikALP <fred05k3@stud.kea.dk>
2022-06-21 11:56:27 +0200

    initial things, middleware+others

--- server/app.js
+++ server/app.js
@@ -1,6 +1,33 @@
+import path from "path";
+
+//Express
 import express from "express";
 const app = express();
+app.use(express.static(path.resolve("../client/public")));
+app.use(express.urlencoded({extended: true}));
 
+//Mongoose
+import mongoose from "mongoose";
+
+//Session
+import session from "express-session";
+
+//Rate-limit
+import rateLimit from "express-rate-limit";
+
+//Helmet
+import helmet from "helmet";
+app.use(helmet());
+
+//Cors
+import cors from "cors";
+app.use(cors());
+
+//Body parser
+import bodyParser from "body-parser";
+app.use(bodyParser.json());
+
+//Port
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log("Starting server on port:", PORT);
