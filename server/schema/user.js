a122cca8836181ee54045286ac4464149d6239ea
FrederikALP <fred05k3@stud.kea.dk>
2022-06-22 14:48:24 +0200

    mongodb virker

--- server/schema/user.js
+++ server/schema/user.js
@@ -4,7 +4,9 @@ const UserSchema = new mongoose.Schema(
 	{
 		username: { type: String, required: true },
 		password: { type: String, required: true },
-        email: { type: String, required: true }
+        email: { type: String, required: true },
+        postcount: { type: Number, required: false },
+        role: { type: Number, required: false }
 	},
 	{ collection: 'users' }
 )
