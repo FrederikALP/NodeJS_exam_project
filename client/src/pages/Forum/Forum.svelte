7d763bf35f3e083677ba717449b6d9d5dab8cbe2
FrederikALP <fred05k3@stud.kea.dk>
2022-06-22 14:33:47 +0200

    single så den opdaterer

--- client/src/pages/Forum/Forum.svelte
+++ client/src/pages/Forum/Forum.svelte
@@ -1,3 +1,6 @@
 <div>
     <h1>Forum WIP</h1>
+    <h2>Opdaterer den ordentligt</h2>
 </div>
+
+<span>Testing igen</span>
--- client/src/pages/Frontpage/Frontpage.svelte
+++ client/src/pages/Frontpage/Frontpage.svelte
@@ -1,3 +1,5 @@
 <div>
     <h1>Frontpage WIP</h1>
+    <h2>Jeg har lavet en ændring</h2>
+    <h3>Endnu en test</h3>
 </div>
