7ad8bab86d04c7432932fca71453cbdb36a968fe
KristianHjelmsmarkKEA <kris69j6@stud.kea.dk>
2022-06-23 14:04:49 +0200

    Backend Crud for posts and comments

--- server/routers/commentRouter.js
+++ server/routers/commentRouter.js
@@ -1,1 +1,67 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
+const router = Router();
+import Comments from "../schema/comments.js";
+import Posts from "../schema/posts.js";
+ 
+//Get all Comments
+router.get("/comments", async (request, response) => {
+    const posts = await Comments.find({});
+
+    try {
+        response.send(Comments);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+//Get comments by post id
+router.get("/commentsByPost/:id", async (request, response) => {
+    const commentsByPost = await Comments.find(Posts.find({id}));
+
+    try {
+        response.send('Comments by post: ', Comments);
+    } catch (error) {
+        response.status(500).send(error);
+    } 
+});
+
+//Create comment on post id
+router.post("/comment", async (request, response) => {
+    const { commentbody, replynumber, postid, user } = req.body;
+
+    try {
+        const response = await Comments.create({
+            commentbody, 
+            replynumber, 
+            postid, 
+            user
+        });
+        console.log('Comment created succesfully: ', response);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+//Update comment by id
+router.patch("/comment/:id", async (request, response) => {
+    try {
+        await Comments.findByIdAndUpdate(request.params.id, requestbody);
+        await Comments.bulkSave();
+        response.send('Comment was updated: ', Comments);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+//Delete comment by id
+router.delete("/comment/:id", async (request, response) => {
+    try {
+        const comment = await Comments.findByIdAndDelete(request.params.id);
+        if (!comment) response.status(404).send('No comment found');
+        response.status(200).send();
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+
--- server/routers/postRouter.js
+++ server/routers/postRouter.js
@@ -1,1 +1,69 @@
-import { Router } from "express";
+import { request, response, Router } from "express";
+const router = Router();
+import Posts from "../schema/posts.js";
+import SubForum from "../schema/subForum.js";
+ 
+//Get all posts
+router.get("/posts", async (request, response) => {
+    const posts = await Posts.find({});
+
+    try {
+        response.send(Posts);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+//Get post by subForum id
+router.get("/postsBySubforum/:id", async (request, response) => {
+    const postsBySubforum = await Posts.find(SubForum.find({id}));
+
+    try {
+        response.send(Posts);
+    } catch (error) {
+        response.status(500).send(error);
+    } 
+});
+
+//Create post
+router.post("/post", async (request, response) => {
+    const { postHeader, date, postbody, replycount, subid, user } = req.body;
+
+    try {
+        const response = await Posts.create({
+            postsHeader,
+            date,
+            postbody,
+            replycount,
+            subid,
+            user
+        });
+        console.log('Post created succesfully: ', response);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+//Update post by id
+router.patch("/post/:id", async (request, response) => {
+    try {
+        await Posts.findByIdAndUpdate(request.params.id, requestbody);
+        await Posts.bulkSave();
+        response.send('Post was updated: ', Posts);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+//Delete post by id
+router.delete("/post/:id", async (request, response) => {
+    try {
+        const post = await Posts.findByIdAndDelete(request.params.id);
+        if (!post) response.status(404).send('No post found');
+        response.status(200).send();
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+
