8e52eae5ac2be313d141c8b01bcc62110c3a79b2
KristianHjelmsmarkKEA <kris69j6@stud.kea.dk>
2022-06-23 13:16:29 +0200

    Router files

--- client/src/pages/Forum/Forum.svelte
+++ client/src/pages/Forum/Forum.svelte
@@ -6,13 +6,8 @@
     <h1>Forum</h1>
     {#if ($user.loggedIn)}
     <h1>Welcome {$user.currentUser.username}</h1>
-<<<<<<< HEAD
     {/if}
     
-=======
-    Virker default fallback
-{/if}
->>>>>>> 86bbc7b4b0a0a7b49c85ecc9e419f904a65e2647
 </div>
 
 <span>Testing igen</span>
