de709999b5d2fc8e37a272a3203fe8c9e88fec2c
KristianHjelmsmarkKEA <kris69j6@stud.kea.dk>
2022-06-23 15:29:04 +0200

    Forum sveltepage

--- client/src/pages/Forum/Forum.svelte
+++ client/src/pages/Forum/Forum.svelte
@@ -1,13 +1,24 @@
 <script>
     import { user } from "../../stores/generalStore.js";
+
+
+    import { onMount } from 'svelte';
+    let forumsHeader;
+
+    onMount(async () => {
+        const response = await fetch('http://localhost:3000/forums');
+        const { data:forumsArray } = await response.json();
+        forumsHeader = forumsArray;
+        console.log(forumsArray);
+        console.log(forumsHeader);
+    })
 </script>
 
 <div>
     <h1>Forum</h1>
+    {$forumsHeader}
     {#if ($user.loggedIn)}
     <h1>Welcome {$user.currentUser.username}</h1>
     {/if}
     
 </div>
-
-<span>Testing igen</span>
